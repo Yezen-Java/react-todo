5c957aac7799996f42183419b143730361d3ea48
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-08-27 15:38:49 +0100

    Add todo component

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var TodoApp = React.createClass({
+
+  render: function () {
+    return(
+      <div>
+        TodoApp.jsx One
+      </div>
+    )
+  }
+});
+
+module.exports = TodoApp;
