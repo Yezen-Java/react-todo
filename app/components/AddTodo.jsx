73ebe66238c8c8762b3f49281e2314e746993d5c
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-08-27 19:49:02 +0100

    create AddTodo component

---
+++ app/components/AddTodo.jsx
@@ -0,0 +1,30 @@
+var React = require('react');
+
+var AddTodo = React.createClass({
+
+onSubmit: function (e) {
+  e.preventDefault();
+ var AddTodoInput = this.refs.TodoInput.value;
+ if(AddTodoInput.length >0){
+   this.refs.TodoInput.value = '';
+   this.props.handleAddTodo(AddTodoInput);
+ }else{
+   this.refs.TodoInput.focus();
+ }
+},
+
+render: function () {
+
+  return(
+    <div>
+      <form ref="form" onSubmit={this.onSubmit} className="countdown-form">
+        <input type="text" ref="TodoInput" placeholder="What do you need to do"/>
+        <button className="button expanded">Add Todo</button>
+      </form>
+    </div>
+  )
+}
+
+});
+
+module.exports = AddTodo;
