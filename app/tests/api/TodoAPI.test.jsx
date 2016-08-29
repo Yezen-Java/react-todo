d71422aa6effca333172d3de09d8dd8a897a1191
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-08-29 19:12:25 +0100

    add filter functionalituy

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -53,4 +53,47 @@ describe('TodoAPI',()=>{
 
   });
 
+  describe('filterTodos',()=>{
+    var todos = [{
+      id:1,
+      text: 'some text here',
+      completed:true
+    },{
+      id:2,
+      text: 'Other text here',
+      completed:false
+    },{
+      id:3,
+      text: 'some text here',
+      completed:true
+    }];
+
+    it('should return all items if showCompleted is true',()=>{
+      var filteredTodos = TodoAPI.filterTodos(todos,true,'');
+      expect(filteredTodos.length).toBe(3);
+    });
+
+    it('should return all items if showCompleted is true',()=>{
+      var filteredTodos = TodoAPI.filterTodos(todos,false,'');
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it('should sort by completed status',()=>{
+      var filteredTodos = TodoAPI.filterTodos(todos,false,'');
+      expect(filteredTodos[0].completed).toBe(false);
+    });
+
+    it('should filterTodos by searchText',()=>{
+      var filteredTodos = TodoAPI.filterTodos(todos,true,'some');
+      expect(filteredTodos.length).toBe(2);
+    });
+
+    it('should return all todos when search text is empty',()=>{
+      var filteredTodos = TodoAPI.filterTodos(todos,true,'');
+      expect(filteredTodos.length).toBe(3);
+    });
+
+
+  });
+
 });
